d0adfcf4df0ac418e4275d4b295f27286f9c5004
Tiago Calogero <tiago@5andhalf.com>
2019-09-19 11:55:31 +1000

    Test logger

--- Logger/GLogger.php
+++ Logger/GLogger.php
@@ -2,10 +2,7 @@
 
 namespace Ipaas\Gapp\Logger;
 
-use Closure;
-use Exception;
 use Google\Cloud\Logging\LoggingClient;
-use Google\Cloud\Logging\PsrLogger;
 use Monolog\Handler\PsrHandler;
 use Monolog\Logger;
 
@@ -20,38 +17,7 @@ class GLogger
         $logName = isset($config['logName']) ? $config['logName'] : 'app';
         $psrLogger = LoggingClient::psrBatchLogger($logName);
         $handler = new PsrHandler($psrLogger);
-        $logger = new Logger(
-            $logName,
-            [$handler],
-            $this->getProcessor($psrLogger)
-        );
-
+        $logger = new Logger($logName, [$handler]);
         return $logger;
-    }
-
-    /**
-     * @param  PsrLogger  $psrLogger
-     * @return Closure[]
-     */
-    public function getProcessor(PsrLogger $psrLogger)
-    {
-        return [
-            function ($record) use ($psrLogger) {
-
-                try { // try context from helper in-case helper is not loaded
-                    $context = ilog()->toArray();
-                } catch (Exception $exception) {
-                    $context = "Unable to get context";
-                }
-
-                return $record['context'] += [
-                    'serviceContext' => [
-                        'service' => $psrLogger->getMetadataProvider()->serviceId(),
-                        'version' => $psrLogger->getMetadataProvider()->versionId(),
-                    ],
-                    'context' => $context
-                ];
-            }
-        ];
     }
 }
